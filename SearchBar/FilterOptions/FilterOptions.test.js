daeffadb2d51bc434a5f203700a52e17672cb7f4
Kevin Damian Ta <kevindamianta@gmail.com>
2019-08-25 21:30:25 -0700

    Adding test, and css files for FilterOptions component

---
+++ SearchBar/FilterOptions/FilterOptions.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer'
+import FilterOptions from './FilterOptions'
+
+describe('FilterOptions component', () =>
+)
