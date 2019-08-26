315253fbb7d2588fe852ee49eac8a9402eac4e84
Kevin Damian Ta <kevindamianta@gmail.com>
2019-08-25 21:31:29 -0700

    SearchBox Component
    Another child component under SearchBar

---
+++ SearchBar/SearchBox/SearchBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SearchBox.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+
+class SearchBox extends React.Component {
+    render() {
+        return(
+            <div className="SearchBox">
+                <FontAwesomeIcon icon={faSearch}/>
+                <input placeholder="Search term" value={this.props.searchTerm}/>
+            </div> 
+        )
+    }
+}
+
+export default SearchBox
