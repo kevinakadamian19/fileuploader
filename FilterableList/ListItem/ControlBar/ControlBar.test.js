98e66d00e05a5f0b52ad935216659dbfefde6982
Kevin Damian Ta <kevindamianta@gmail.com>
2019-08-25 21:38:30 -0700

    Update ControlBar.test.js

--- FilterableList/ListItem/ControlBar/ControlBar.test.js
+++ FilterableList/ListItem/ControlBar/ControlBar.test.js
@@ -1,1 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+import ControlBar from './ControlBar'
 
+describe('ControlBar component', () => {
+    
+})
