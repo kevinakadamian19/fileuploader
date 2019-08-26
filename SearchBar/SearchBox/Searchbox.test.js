07ad3e780c4e8183ebd3a5f50bd423ce18dae32b
Kevin Damian Ta <kevindamianta@gmail.com>
2019-08-25 21:32:27 -0700

    Test, and CSS files for component

---
+++ SearchBar/SearchBox/Searchbox.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer'
+import SearchBox from './SearchBox'
+
+describe('SearchBox component', () =>
+)
