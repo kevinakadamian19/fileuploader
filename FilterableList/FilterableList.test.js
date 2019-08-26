1f6d9b84136d4dd0cfd12b857c14d3282a12d8f2
Kevin Damian Ta <kevindamianta@gmail.com>
2019-08-25 21:35:32 -0700

    Test, and Css files
    These are the test and CSS files

---
+++ FilterableList/FilterableList.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import FilterableList from './FilterableList'
+import renderer from 'react-test-renderer'
+
+describe('FilterableList component',() => {
+    
+})
